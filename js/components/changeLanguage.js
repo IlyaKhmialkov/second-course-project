91fe23588e7784eec3f0aa0b16ec4fc9702609d0
Ilya K <gromokotgrom@gmail.com>
2024-05-11 18:58:30 +0300

    feat: add page translation into Japanese, add new font to better view of page in Japanese, add styles to burger menu

---
+++ js/components/changeLanguage.js
@@ -0,0 +1,37 @@
+import { translate } from "../../assets/translate.js";
+const languageButton = document.getElementById("change-language");
+
+function changeLanguage(language){
+    const translation = translate[language];
+
+    languageButton.textContent = language;
+
+    const elements = document.querySelectorAll('[data-i18]');
+
+    elements.forEach((element) => {
+        const key = element.getAttribute('data-i18');
+        const translatedText = translation[key];
+
+        element.textContent = translatedText;
+    });
+}
+
+export function changeLanguageSetup(){
+    const defalutLanguage = localStorage.getItem('language') ?? 'en';
+
+    if(defalutLanguage === 'ja'){
+        changeLanguage('ja');
+    } else {
+        changeLanguage('en');
+    }
+
+    languageButton.addEventListener('click', function(){
+        if(languageButton.textContent === 'en'){
+            changeLanguage('ja');
+            localStorage.setItem('language', 'ja');
+        } else {
+            changeLanguage('en');
+            localStorage.setItem('language', 'en');
+        }
+    })
+}
--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ signUpButton.addEventListener('click', function() {
 import { sliderSetup } from '/js/components/slider.js';
 import { burgerSetup } from '/js/components/burger-menu.js';
 import { darkThemeSetup } from '/js/components/dark-theme.js';
+import { changeLanguageSetup } from '/js/components/changeLanguage.js';
 
 async function main(){
     burgerSetup();
     darkThemeSetup();
     await sliderSetup();
+    changeLanguageSetup();
 }
 
 main();
